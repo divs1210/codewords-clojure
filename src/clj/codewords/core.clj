a3a1776ccfe78685b38897da7fd198cf0b66c630
Divyansh Prakash <divyansh@redpanda.co.in>
2016-02-21 06:47:17 +0530

    Moved figwheel startup to codewords.user

--- src/clj/codewords/core.clj
+++ src/clj/codewords/core.clj
@@ -1,6 +1,1 @@
-(ns codewords.core
-  (:use [figwheel-sidecar.repl-api]))
-
-;; Start dev server
-;; ================
-(start-figwheel!)
+(ns codewords.core)
---
+++ src/clj/codewords/user.clj
@@ -0,0 +1,6 @@
+(ns codewords.user 
+  (:use [figwheel-sidecar.repl-api]))
+
+;; Start dev server
+;; ================
+(start-figwheel!)
