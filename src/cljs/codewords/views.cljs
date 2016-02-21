957287f47ec346a939acd24deda3452275276a4a
Divyansh Prakash <divyansh@redpanda.co.in>
2016-02-21 12:58:19 +0530

    Added footer

--- src/cljs/codewords/views.cljs
+++ src/cljs/codewords/views.cljs
@@ -9,10 +9,15 @@
   [:div#timer
    [:p "seconds elapsed: " seconds]])
 
+(defn footer-comp []
+  [:div#footer {:style {:background "#cce"}}
+   [:p "Codewords"]])
+
 (defn main-panel []
   (let [seconds (re-frame/subscribe [:seconds])]
     (fn []
       [:div#main-panel {:style {:text-align "center"
                                 :background "#efe"}}
        [header-comp]
-       [timer-comp @seconds]])))
+       [timer-comp @seconds]
+       [footer-comp]])))
