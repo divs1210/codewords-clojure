a8bb62997ee5a45a243baa635bfeb049823b9522
Red Panda <divyansh.prakash@yahoo.com>
2016-03-11 04:10:30 +0530

    minor refactoring

--- src/cljs/codewords/views.cljs
+++ src/cljs/codewords/views.cljs
@@ -5,14 +5,14 @@
   {:dark  "#99f"
    :light "#9f9"})
 
-(defn header []
+(defn header [text]
   [:div#header
    [:p {:style {:background (:dark colors)}}
-    "Codewords Demo"]])
+    text]])
 
-(defn footer []
+(defn footer [text]
   [:div#footer {:style {:background (:dark colors)}}
-   [:p "March 2016"]])
+   [:p text]])
 
 (defn timer []
   (let [seconds (rf/subscribe [:seconds])]
@@ -20,34 +20,34 @@
       [:div#timer
        [:p "seconds elapsed: " @seconds]])))
 
-(defn cell [row col width%]
+(defn cell [width row col]
   (let [hidden? (rf/subscribe [:hidden? [row col]])]
     (fn []
-      [:td {:style {:width width%
+      [:td {:style {:width width
                     :background (if @hidden?
                                   (:light colors)
                                   (:dark colors))}
             :on-click (fn []
                         (rf/dispatch [:cell-clicked [row col]]))}])))
 
-(defn row-of-cells [row cols height% width%]
-  [:tr {:style {:height height%}}
+(defn row-of-cells [height width row cols]
+  [:tr {:style {:height height}}
    (for [col (range cols)]
      ^{:key (str "col-" col)}
-     [cell row col width%])])
+     [cell width row col])])
 
-(defn grid [rows cols]
-  (let [height% (/ 100 rows)
-        width%  (/ 100 cols)]
+(defn grid [height width rows cols]
+  (let [row-height (/ height rows)
+        col-width  (/ width  cols)]
     [:div#grid>table {:style {:margin "auto"}}
      (for [row (range rows)]
        ^{:key (str "row-" row)}
-       [row-of-cells row cols height% width%])]))
+       [row-of-cells row-height col-width row cols])]))
 
 (defn main-panel []
   [:div#main-panel {:style {:text-align "center"
                             :background (:light colors)}}
-   [header]
-   [grid 5 5]
+   [header "CodeWords Demo"]
+   [grid 100 100 5 5]
    [timer]
-   [footer]])
+   [footer "March 2016"]])
